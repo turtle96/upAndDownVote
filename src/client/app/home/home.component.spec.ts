d069f2174525a4f31291cd242ffc35f5472302e7
turtle96 <angjazlyn@hotmail.com>
2017-09-10 01:24:12 +0800

    add tests: - read topics, enter username, enter new topic - up votes, down votes, sort top 20 - sort all topics by alphabetical order

--- src/client/app/home/home.component.spec.ts
+++ src/client/app/home/home.component.spec.ts
@@ -24,7 +24,7 @@ export function main() {
 
     });
 
-    it('should be able to read topics from data.json, enter username',
+    it('should be able to read topics from data.json, enter username, enter new topic',
       async(() => {
         TestBed
           .compileComponents()
@@ -37,7 +37,7 @@ export function main() {
             let homeServiceSpy = spyOn(mockHomeService, 'get').and.callThrough();
 
             mockHomeService.topics = [
-              {name: "Animals", votes: 20, user: "duck_duck"},
+              {name: "Animals", votes: 50, user: "duck_duck"},
               {name: "Capitalism", votes: 50, user: "duck_duck"},
               {name: "Guitar", votes: 40, user: "duck_duck"}
             ];
@@ -46,7 +46,6 @@ export function main() {
 
             expect(homeInstance.homeService).toEqual(jasmine.any(MockHomeService));
             expect(homeDOMEl.querySelectorAll('li').length).toEqual(6);
-            // expect(homeServiceSpy.calls.count()).toBe(1);
 
             homeInstance.newName = 'MapleSyrup';
             homeInstance.enterUsername();
@@ -55,10 +54,124 @@ export function main() {
 
             expect(homeDOMEl.querySelectorAll('h5')[1].textContent).toEqual('Logged in as: MapleSyrup');
 
+            homeInstance.newTopic = 'Pancakes and Honey';
+            homeInstance.enterNewTopic();
+
             fixture.detectChanges();
 
-            // expect(homeDOMEl.querySelectorAll('li').length).toEqual(4);
-            // expect(homeDOMEl.querySelectorAll('li')[3].textContent).toEqual('Minko');
+            expect(homeDOMEl.querySelectorAll('li').length).toEqual(9);
+            expect(homeDOMEl.querySelectorAll('li')[0].textContent).toEqual('Topic Accepted');
+
+            // has comparison issues due to HTML formatting, so add a trim()
+            expect(homeDOMEl.querySelectorAll('li')[4].textContent.trim()).toEqual('4. Topic: Pancakes and Honey, Votes: 0, Submitted by: MapleSyrup');
+
+          });
+
+      }));
+
+    it('should be able to up vote, resort top 20',
+      async(() => {
+        TestBed
+          .compileComponents()
+          .then(() => {
+            let fixture = TestBed.createComponent(HomeComponent);
+            let homeInstance = fixture.debugElement.componentInstance;
+            let homeDOMEl = fixture.debugElement.nativeElement;
+            let mockHomeService =
+              fixture.debugElement.injector.get<any>(HomeService) as MockHomeService;
+
+            mockHomeService.topics = [
+              {name: "Animals", votes: 50, user: "duck_duck"},
+              {name: "Capitalism", votes: 50, user: "duck_duck"},
+              {name: "Guitar", votes: 40, user: "duck_duck"}
+            ];
+
+            fixture.detectChanges();
+
+            expect(homeInstance.homeService).toEqual(jasmine.any(MockHomeService));
+
+            homeInstance.newName = 'MapleSyrup';
+            homeInstance.enterUsername();
+
+            fixture.detectChanges();
+
+            expect(homeDOMEl.querySelectorAll('h5')[1].textContent).toEqual('Logged in as: MapleSyrup');
+
+            expect(homeDOMEl.querySelectorAll('li')[0].textContent.trim()).toEqual('1. Topic: Animals, Votes: 50, Submitted by: duck_duck');
+
+            homeInstance.upVote(mockHomeService.topics[1]); // up vote Capitalism
+
+            fixture.detectChanges();
+
+            expect(homeDOMEl.querySelectorAll('li')[0].textContent.trim()).toEqual('1. Topic: Capitalism, Votes: 51, Submitted by: duck_duck');
+          });
+
+      }));
+
+    it('should be able to down vote, resort top 20',
+      async(() => {
+        TestBed
+          .compileComponents()
+          .then(() => {
+            let fixture = TestBed.createComponent(HomeComponent);
+            let homeInstance = fixture.debugElement.componentInstance;
+            let homeDOMEl = fixture.debugElement.nativeElement;
+            let mockHomeService =
+              fixture.debugElement.injector.get<any>(HomeService) as MockHomeService;
+
+            mockHomeService.topics = [
+              {name: "Animals", votes: 50, user: "duck_duck"},
+              {name: "Capitalism", votes: 50, user: "duck_duck"},
+              {name: "Guitar", votes: 40, user: "duck_duck"}
+            ];
+
+            fixture.detectChanges();
+
+            expect(homeInstance.homeService).toEqual(jasmine.any(MockHomeService));
+
+            homeInstance.newName = 'MapleSyrup';
+            homeInstance.enterUsername();
+
+            fixture.detectChanges();
+
+            expect(homeDOMEl.querySelectorAll('h5')[1].textContent).toEqual('Logged in as: MapleSyrup');
+
+            expect(homeDOMEl.querySelectorAll('li')[0].textContent.trim()).toEqual('1. Topic: Animals, Votes: 50, Submitted by: duck_duck');
+
+            homeInstance.downVote(mockHomeService.topics[0]); // down vote Animals
+
+            fixture.detectChanges();
+
+            expect(homeDOMEl.querySelectorAll('li')[0].textContent.trim()).toEqual('1. Topic: Capitalism, Votes: 50, Submitted by: duck_duck');
+            expect(homeDOMEl.querySelectorAll('li')[1].textContent.trim()).toEqual('2. Topic: Animals, Votes: 49, Submitted by: duck_duck');
+          });
+
+      }));
+
+    it('should be able to sort by alphabetical order',
+      async(() => {
+        TestBed
+          .compileComponents()
+          .then(() => {
+            let fixture = TestBed.createComponent(HomeComponent);
+            let homeInstance = fixture.debugElement.componentInstance;
+            let homeDOMEl = fixture.debugElement.nativeElement;
+            let mockHomeService =
+              fixture.debugElement.injector.get<any>(HomeService) as MockHomeService;
+
+            mockHomeService.topics = [
+              {name: "Capitalism", votes: 50, user: "duck_duck"},
+              {name: "Guitar", votes: 40, user: "duck_duck"},
+              {name: "Animals", votes: 50, user: "duck_duck"}
+            ];
+
+            expect(homeInstance.homeService).toEqual(jasmine.any(MockHomeService));
+
+            fixture.detectChanges();
+
+            expect(homeDOMEl.querySelectorAll('li')[3].textContent.trim()).toEqual('Topic: Animals, Votes: 50, Submitted by: duck_duck');
+            expect(homeDOMEl.querySelectorAll('li')[4].textContent.trim()).toEqual('Topic: Capitalism, Votes: 50, Submitted by: duck_duck');
+            expect(homeDOMEl.querySelectorAll('li')[5].textContent.trim()).toEqual('Topic: Guitar, Votes: 40, Submitted by: duck_duck');
           });
 
       }));

d690b4d6926b6cdede04b2d3c5856caf5d43291b
turtle96 <angjazlyn@hotmail.com>
2017-09-15 22:13:54 +0800

    console log

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+
+console.log(process.env.PWD + '/dist/prod/');
+console.log(__dirname);
+
 app.use(express.static(process.env.PWD + '/dist/prod/'));
 
 app.listen(process.env.PORT || 3000);

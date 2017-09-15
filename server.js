6dea8e00ab9e5983fd57ae08147dca5b4924bf5f
turtle96 <angjazlyn@hotmail.com>
2017-09-15 22:18:12 +0800

    try this

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 console.log(process.env.PWD + '/dist/prod/');
 console.log(__dirname);
 
-app.use(express.static(process.env.PWD + '/dist/prod/'));
+process.env.PWD = process.cwd();
+
+app.use(express.static(process.env.PWD + '/dist/prod'));
 
 app.listen(process.env.PORT || 3000);
